2a32209a19ba46215ea6aa1d9fa6e5b7788591dd
Louise Sinnott <louisesinnott94@gmail.com>
2014-11-17 18:40:48 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
+
 class UsersController < ApplicationController
-  def new
-  end
-end
+      def show
+        @user = User.find(params[:id])
+        #@microposts = @user.microposts    # NEW LINE
+      end
+
+      def new
+        @user = User.new
+      end
+
+      
+
+      def create
+
+        secure_params = params.require(:user).permit(:student_no, :name, :email, :password, :password_confirmation)
+        @user = User.new(secure_params)
+        
+        if @user.save
+        	sign_in @user       #  NEW LINE
+        	flash[:success] = "Welcome to the Sample App!"    # NEW LINE
+        	redirect_to @user   # NEW LINE
+        else
+            render 'new'     # NEW LINE    
+        end
+      end
+    end
+
+
