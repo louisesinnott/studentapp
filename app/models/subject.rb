f837ef436ad7d62292552f23b182a97014b7a592
Louise Sinnott <louisesinnott94@gmail.com>
2014-12-05 15:10:02 +0000

    Finish subject filtering

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -3,7 +3,7 @@ has_many :feedbacks #association with feedback
 has_and_belongs_to_many :courses
 
 
-	validates :subject_name, presence:true
+	validates :name, presence:true
 	validates :description, presence:true
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# Create some course
-   Course.create(course_id:'WD165', name:'Bsc (h) in Multimedia')
-   Course.create(course_id:'WD153', name:'Bsc in Multimedia')
-   Course.create(course_id:'WD155', name:'Bsc (h) in IT')
-   Course.create(course_id:'WD167', name:'Bsc (h) in SSD')
+
+
+graphic = Subject.create(name: 'Graphic Design', description: 'Design', ca_percent: '100', final_exam_percent: '0')
+wsp = Subject.create(name: 'WebServer', description: 'Programming', ca_percent: '100', final_exam_percent: '0')
+network = Subject.create(name: 'Networks', description: 'Networks', ca_percent: '50', final_exam_percent: '50')
+
+    # Associate some tags with microposts
+    Course.find(1).subjects << graphic
+    Course.find(1).subjects << wsp
+    Course.find(2).subjects << network
     
-   
+    
